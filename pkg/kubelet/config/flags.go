6f9b7c7a161e60cf44b57b0f1ac8e888f33d55e6
Lantao Liu <lantaol@google.com>
2018-08-20 18:11:03 -0700

    Add gvisor-node-mode flag.
    Change-Id: I97a305ab520b2440a65054dc5fa77db30f3d2a15

--- pkg/kubelet/config/flags.go
+++ pkg/kubelet/config/flags.go
@@ -40,6 +40,11 @@ type ContainerRuntimeOptions struct {
 	// but less secure because the connection between apiserver and container runtime is not
 	// authenticated.
 	RedirectContainerStreaming bool
+	// GVisorNodeMode enables gvisor node mode. In this mode, kubelet rejects exec/attach to
+	// non-gvisor pods.
+	// TODO(b/113282521): Remove this mode and allow exec/attach into non-gvisor pods after
+	// XEMU is open sourced.
+	GVisorNodeMode bool
 
 	// Docker-specific options.
 
