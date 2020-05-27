afe86e9d2fca12d2976c304ad48b9942e5e5b50b
Samuel Davidson <samueldavidson@google.com>
2020-05-27 16:57:33 -0700

    Cherrypick SNI Flags (#91228) to 1.18 for go/gke-cred-rotation-sni
    https://github.com/kubernetes/kubernetes/pull/91228
    
    b/156542212
    
    (edit-for-retest-1)
    
    Change-Id: Ibbd0ef7750a45ebe2554ba2817ee60e456cfc821

--- cluster/gce/gci/configure-kubeapiserver.sh
+++ cluster/gce/gci/configure-kubeapiserver.sh
@@ -80,8 +80,6 @@ function start-kube-apiserver {
     # Default is :8080
     params+=" --insecure-port=0"
   fi
-  params+=" --tls-cert-file=${APISERVER_SERVER_CERT_PATH}"
-  params+=" --tls-private-key-file=${APISERVER_SERVER_KEY_PATH}"
   params+=" --kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname"
   if [[ -s "${REQUESTHEADER_CA_CERT_PATH:-}" ]]; then
     params+=" --requestheader-client-ca-file=${REQUESTHEADER_CA_CERT_PATH}"
@@ -93,6 +91,15 @@ function start-kube-apiserver {
     params+=" --proxy-client-key-file=${PROXY_CLIENT_KEY_PATH}"
   fi
   params+=" --enable-aggregator-routing=true"
+  params+=" --tls-cert-file=${APISERVER_SERVER_CERT_PATH}"
+  params+=" --tls-private-key-file=${APISERVER_SERVER_KEY_PATH}"
+  if [[ -n "${OLD_MASTER_IP:-}" ]]; then
+    local old_ips="${OLD_MASTER_IP}"
+    if [[ -n "${OLD_LOAD_BALANCER_IP}" ]]; then
+      old_ips+=",${OLD_LOAD_BALANCER_IP}"
+    fi
+    params+=" --tls-sni-cert-key=${OLD_MASTER_CERT_PATH},${OLD_MASTER_KEY_PATH}:${old_ips}"
+  fi
   if [[ -e "${APISERVER_CLIENT_CERT_PATH}" ]] && [[ -e "${APISERVER_CLIENT_KEY_PATH}" ]]; then
     params+=" --kubelet-client-certificate=${APISERVER_CLIENT_CERT_PATH}"
     params+=" --kubelet-client-key=${APISERVER_CLIENT_KEY_PATH}"
